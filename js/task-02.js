227a3854ef480022c27afd93f2a71129c89c4608
eduard-kaluhin <122424824+eduard-kaluhin@users.noreply.github.com>
2023-05-08 17:29:41 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+
+  const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
+  const ul = document.querySelector('#ingredients');
+  const fragment = document.createDocumentFragment();
+  
+  ingredients.forEach(ingredient => {
+    const li = document.createElement('li');
+    li.textContent = ingredient;
+    li.classList.add('item');
+    fragment.appendChild(li);
+  });
+  
+  ul.appendChild(fragment);
+
