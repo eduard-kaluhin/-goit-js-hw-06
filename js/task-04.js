465cead006ea62459d1f9eea3b07853eb2dd380a
eduard-kaluhin <122424824+eduard-kaluhin@users.noreply.github.com>
2023-05-17 10:58:33 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const counterValueEl = document.querySelector("#value");
+
+let counterValue = 0;
+
+decrementBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  counterValueEl.textContent = counterValue;
+});
+
+incrementBtn.addEventListener("click", () => {
+  counterValue += 1;
+  counterValueEl.textContent = counterValue;
+});
