083a84cdfd2eba8cce504a7cec222f32011325f2
eduard-kaluhin <122424824+eduard-kaluhin@users.noreply.github.com>
2023-05-17 18:26:15 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,8 @@ const categories = categoriesList.querySelectorAll('.item');
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  const categoryElements = category.querySelectorAll('li').length;
+  const categoryName = category.children[0].textContent;
+  const categoryElements = category.children[1].children.length;
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements}`);
 });
