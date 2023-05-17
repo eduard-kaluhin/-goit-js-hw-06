773454ea872996ef996345fd76e1f182563e228c
eduard-kaluhin <122424824+eduard-kaluhin@users.noreply.github.com>
2023-05-17 10:58:38 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,4 @@
+const galleryList = document.querySelector(".gallery");
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const galleryMarkup = images
+  .map(({ url, alt }) => {
+    return `<li class="gallery-item"><img src="${url}" alt="${alt}" class="gallery-image"></li>`;
+  })
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
