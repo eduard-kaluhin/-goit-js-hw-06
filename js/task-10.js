d6ec4a70bbc7af6d0ed9223093bebcacba370c19
eduard-kaluhin <122424824+eduard-kaluhin@users.noreply.github.com>
2023-05-17 10:58:09 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,33 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    boxSize += 10;
+  }
 }
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+createBtn.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
