7cc5124054e07eabf8e9a54817c2086eb8023edf
eduard-kaluhin <122424824+eduard-kaluhin@users.noreply.github.com>
2023-05-17 10:58:15 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+  form.addEventListener('submit', handleSubmit);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+      alert('All fields must be filled in!');
+    } else {
+      const formData = {
+        email: email.value,
+        password: password.value,
+      };
+      console.log(formData);
+      form.reset();
+    }
+  }
